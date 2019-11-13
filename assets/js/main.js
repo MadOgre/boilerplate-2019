128ae5ce35a4211b0e596f3461ea9fc25c6fe2d4
Mike Semko <msemko@gmail.com>
2019-11-13 13:14:34 -0500

    Add html-webpack-plugin and babel-loader with plg

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
-console.log("this works");
+class Test {
+  x = 34;
+  getX = () => {
+    return this.x;
+  }
+}
+
+let x = new Test;
+
+console.log(x.getX());
--- webpack.config.client.prod.js
+++ webpack.config.client.prod.js
@@ -1,10 +1,30 @@
 const { resolve } = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
+  template: "./assets/index.ejs"
+});
 
 module.exports = {
-  entry: "./assets/js/main.js",
+  entry: ["@babel/polyfill", "./assets/js/main.js"],
   mode: "production",
   output: {
     path: resolve(__dirname, "public"),
     filename: "bundle.js"
-  }
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      use: [{
+        loader: "babel-loader",
+        options: {
+          cacheDirectory: true,
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+          plugins: ["@babel/plugin-proposal-class-properties"]
+        }
+      }]
+    }]
+  },
+  plugins: [
+    htmlWebpackPlugin
+  ]
 };
